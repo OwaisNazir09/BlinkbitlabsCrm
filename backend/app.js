8cf09e8ede1656a46422e176db3b3789be739eaf
Owais Nazir <owaisaalam38@gmail.com>
2025-10-26 22:07:04 +0530

    Resolve merge conflicts

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,7 @@ app.use(morgan("dev"));
 setupRoutes(app);
 app.use(setCookieOptions);
 
-const PORT = process.env.PORT || 5000;
-const HOST = "127.0.0.1";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
